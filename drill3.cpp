5b44b00804b1e54cf4d20411b0a6038a4a52b799
Balázs Tóth <gawnoris@gmail.com>
2018-09-21 22:46:53 +0200

    Update drill3.cpp

--- drill3.cpp
+++ drill3.cpp
@@ -1,19 +1,23 @@
 
 #include "std_lib_facilities.h"
 
+
 int main()
 {
 cout << "Please enter the name of the person you want to write to:\n";
 string first_name; 
+string simple_error="you're kidding!";
+string friend_name;
+char friend_gender = ' ';
+int age;
+string name;
 cin >> first_name;
 cout << "Dear " << first_name << ",\n";
 cout << "How are you? I am fine. I miss you.\n";
 cout << "blah blah\n";
-string friend_name;
 cout<< "write the name of the friend you want to talk about:\n";
 cin >> friend_name;
-cout << "Have you seen" << friend_name << " lately?\n";
-char friend_gender = ' ';
+cout << "Have you seen " << friend_name << " lately?\n";
 cout << "if the friend we are talking about is male, write m, if not, write f";
 cin >> friend_gender;
 if (friend_gender=='m')
@@ -24,17 +28,17 @@ else if (friend_gender=='f')
 {
 cout << "If you see " << friend_name << "please ask her to call me\n";
 }
-int age;
+
 cout << "please enter the age of the recipient:\n";
 cin >> age;
 
 if ( age<=0)
 {
-simple_error("you're kidding!");
+simple_error;
 }
 else if (age>=110)
 {
-simple_error("you're kidding!");
+simple_error;
 }
 else
 { /*open*/ 
@@ -42,7 +46,7 @@ cout << "i heard you just had a birthday and now you are " << age << " years old
 
 if (age<=12) 
 {
-cout << "next year you will be" << age+1 << "\n";
+cout << "next year you will be " << age+1 << "\n";
 }
 else if (age=17)
 {
@@ -54,7 +58,7 @@ cout << "I hope you are enjoying retirement.\n";
 }
 
 /*close*/ }
-string name;
+
 cout<<"please enter your name:";
 cin>> name;
 cout << "\nYours sincerely,\n\n" <<name;
