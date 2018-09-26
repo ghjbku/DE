ad71412933803587e8cd97848a8048a75645090e
Balázs Tóth <gawnoris@gmail.com>
2018-09-26 12:47:30 +0200

    Add files via upload

---
+++ szep25.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+
+
+int main()
+{
+int i=0;
+
+while(i<100)
+{
+cout << i << '\t' << sqrt(i) << '\n';
+++i;
+}
+cout <<'\n';
+for (int i = 0; i<100; ++i) {
+	cout << i << '\t' << square(i) << '\n';
+}	
+}
---
+++ szep25_2.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+
+
+int main()
+{
+
+int i=0;
+
+while(i<100)
+{
+cout << i << '\t' << sqrt(i) << '\n';
+++i;
+}
+cout <<'\n';
+for (i = 0; i<100; ++i) {
+	cout << i << '\t' << sqrt(i) << '\n';
+}	
+}
