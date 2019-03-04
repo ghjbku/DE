3e80123b6965ae12373eb6c00bef3fc23c092f2e
Balázs Tóth <gawnoris@gmail.com>
2019-03-04 20:38:48 +0100

    Update bitshift.cpp

--- bitshift.cpp
+++ bitshift.cpp
@@ -2,9 +2,6 @@
 
 int main()
 {
-	/*2. Mutassuk meg, hogy milyen hosszú a gépünkön egy gépi szó. Használjuk a bitshift operátort. Pl:
-int = 1;
-Számoljuk meg, hogy az a <<= 1; kifejezés hány lépés után lesz hamis (azaz a == 0);*/
 unsigned long long int loops_per_sec = 1;
 int s=0;
 while ((loops_per_sec <<= 1))
